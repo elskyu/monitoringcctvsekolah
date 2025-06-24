e8b6f074ba14f2a3e16cec2f07743f4aad932932
khoirulfikri <fikrikhoirul70@gmail.com>
2025-06-24 13:59:54 +0700

    update:SekolahSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,7 +19,15 @@ class UserSeeder extends Seeder
             'id' => 2,
             'name' => 'admin',
             'email' => 'admin@gmail.com',
-            'password' => Hash::make('1'),
+            'password' => Hash::make('123'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'wiko',
+            'email' => 'wiko@gmail.com',
+            'password' => Hash::make('123'),
             'created_at' => now(),
             'updated_at' => now()
         ]);
